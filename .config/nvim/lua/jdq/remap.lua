8cfa576c07a322aac38f2b9054aaf09f208ef778
Jake Quinter <hello@jakequinter.io>
2023-07-30 08:28:35 -0400

    splits

--- .config/nvim/lua/jdq/remap.lua
+++ .config/nvim/lua/jdq/remap.lua
@@ -8,3 +8,6 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("n", "<leader>v", ":vsplit<CR>")
+vim.keymap.set("n", "<leader>h", ":split<CR>")
