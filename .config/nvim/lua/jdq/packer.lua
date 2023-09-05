7d2e0d021146a217af01bea63fb4be0c5ccaf634
Jake Quinter <hello@jakequinter.io>
2023-09-05 09:43:03 -0400

    lualine

--- .config/nvim/lua/jdq/packer.lua
+++ .config/nvim/lua/jdq/packer.lua
@@ -48,6 +48,11 @@ return require('packer').startup(function(use)
   end 
   })
 
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+  }
+
  use { "j-hui/fidget.nvim", branch = "legacy" }
   
 
