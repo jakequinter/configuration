669f0823fee6705129d381bf39fc6b0563b3e223
Jake Quinter <hello@jakequinter.io>
2024-03-01 09:14:16 -0500

    fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,21 +2,53 @@
 # Fish uses `fish_add_path` instead of `export PATH` modify $PATH.
 fish_add_path "/opt/homebrew/bin/"
 
-set -U neofetch_run_once 0
+# set -U neofetch_run_once 0
+#
+# function fish_greeting -d "Greeting message on shell session start up"
+#   set -l current_pid %self
+#   set -l lowest_pid (ps -eo pid=,args= | string match '*fish*' | awk '{print $1}' | sort -n | head -n 1)
+#
+#   # run neofetch on first shell
+#   if test "$current_pid" -eq "$lowest_pid"
+#     neofetch
+#   end
+# end
 
-function fish_greeting -d "Greeting message on shell session start up"
-  set -l current_pid %self
-  set -l lowest_pid (ps -eo pid=,args= | string match '*fish*' | awk '{print $1}' | sort -n | head -n 1)
-
-  # run neofetch on first shell
-  if test "$current_pid" -eq "$lowest_pid"
-    neofetch
-  end
-end
 set fish_greeting
 
-# Enable Starship prompt
 starship init fish | source
+
+function fish_prompt
+    if set -q fish_should_add_space
+        if test "$fish_should_add_space" = "1"
+            echo
+        end
+    end
+
+    set -l cwd (pwd)
+    set_color normal
+    if test "$cwd" = "$HOME"
+        echo "~"
+    else if test (git rev-parse --is-inside-work-tree 2>/dev/null) = "true"
+        echo (basename $cwd)
+    else
+        echo (string replace -r "^$HOME" '~' $cwd)
+    end
+    set_color "#ffa500"
+    echo -n 'λ '
+    set_color normal
+
+    set -U fish_should_add_space 0
+end
+function prepare_command --on-event fish_preexec
+    if not string match -q "clear" -- $argv
+        set -U fish_should_add_space 1
+    else
+        set -U fish_should_add_space 0
+    end
+end
+
+set -U fish_should_add_space 0
 
 # homebrew path
 set -gx PATH /opt/homebrew/bin $PATH
