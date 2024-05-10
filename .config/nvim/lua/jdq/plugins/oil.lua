4d55ec8d694a6582f4d3a713e97118efd3898291
Jake Quinter <hello@jakequinter.io>
2024-05-10 13:20:46 -0400

    oil

--- .config/nvim/lua/jdq/plugins/oil.lua
+++ .config/nvim/lua/jdq/plugins/oil.lua
@@ -9,7 +9,7 @@ return {
 				["g?"] = "actions.show_help",
 				["<CR>"] = "actions.select",
 				["<C-s>"] = "actions.select_vsplit",
-				["<C-h>"] = "actions.select_split",
+				["<C-h>"] = false,
 				["<C-t>"] = "actions.select_tab",
 				["<C-p>"] = false,
 				["<C-c>"] = "actions.close",
