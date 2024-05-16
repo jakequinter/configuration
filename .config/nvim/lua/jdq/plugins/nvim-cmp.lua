eb69ea6573e56c261a5b2c0381fe6f42abdd4022
Jake Quinter <hello@jakequinter.io>
2024-05-16 08:48:42 -0400

    luasnip

--- .config/nvim/lua/jdq/plugins/nvim-cmp.lua
+++ .config/nvim/lua/jdq/plugins/nvim-cmp.lua
@@ -2,8 +2,10 @@ return {
 	"hrsh7th/nvim-cmp",
 	event = "InsertEnter",
 	dependencies = {
-		"hrsh7th/cmp-buffer",
+		"onsails/lspkind.nvim",
+		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-buffer",
 		"L3MON4D3/LuaSnip",
 		"saadparwaiz1/cmp_luasnip",
 		"rafamadriz/friendly-snippets",
@@ -11,6 +13,8 @@ return {
 	config = function()
 		local cmp = require("cmp")
 		local luasnip = require("luasnip")
+		local lspkind = require("lspkind")
+		lspkind.init({})
 
 		cmp.setup({
 			completion = {
@@ -22,9 +26,23 @@ return {
 				end,
 			},
 			mapping = cmp.mapping.preset.insert({
-				["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-				["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-				["<S-Tab>"] = cmp.mapping(function(fallback)
+				["<C-n>"] = cmp.mapping.select_next_item(), -- next suggestion
+				["<C-p>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+				["<C-u>"] = cmp.mapping.scroll_docs(4), -- scroll up preview
+				["<C-d>"] = cmp.mapping.scroll_docs(-4), -- scroll down preview
+				["<C-Space>"] = cmp.mapping.complete({}), -- show completion suggestions
+				["<C-c>"] = cmp.mapping.abort(), -- close completion window
+				["<CR>"] = cmp.mapping.confirm({ select = true }), -- select suggestion
+				["<C-k>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif luasnip.locally_jumpable(1) then
+						luasnip.jump(1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				["<C-j>"] = cmp.mapping(function(fallback)
 					if cmp.visible() then
 						cmp.select_prev_item()
 					elseif luasnip.jumpable(-1) then
@@ -33,11 +51,6 @@ return {
 						fallback()
 					end
 				end, { "i", "s" }),
-				["<C-u>"] = cmp.mapping.scroll_docs(4), -- scroll up preview
-				["<C-d>"] = cmp.mapping.scroll_docs(-4), -- scroll down preview
-				["<C-Space>"] = cmp.mapping.complete({}), -- show completion suggestions
-				["<C-c>"] = cmp.mapping.abort(), -- close completion window
-				["<CR>"] = cmp.mapping.confirm({ select = true }), -- select suggestion
 			}),
 			sources = cmp.config.sources({
 				{ name = "nvim_lsp" }, -- lsp suggestions
