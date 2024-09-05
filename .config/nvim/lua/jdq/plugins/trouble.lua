281f7c557c317903b318b6eaa50320116a00180b
Jake Quinter <hello@jakequinter.io>
2024-09-05 11:16:11 -0500

    trouble

--- .config/nvim/lua/jdq/plugins/trouble.lua
+++ .config/nvim/lua/jdq/plugins/trouble.lua
@@ -1,9 +1,16 @@
 return {
-  "folke/trouble.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
+	"folke/trouble.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+	},
+	keys = {
+		{
+			"<leader>xx",
+			"<cmd>Trouble diagnostics toggle<cr>",
+			desc = "Diagnostics (Trouble)",
+		},
+	},
 }
